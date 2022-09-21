2d2ec6883a0b7683ea4d51f494ebafcf8786c22f
mayowabadewa <100229789+mayowabadewa@users.noreply.github.com>
2022-09-21 09:26:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ weight = float(input("enter your weight in kg: "))
 
 BMI = round((weight/(height**2)))
 
-if BMI >=35:
+if BMI > 35:
     print(f"Your BMI is {BMI}, you are clinically obese.")
-elif BMI >=30:
+elif BMI > 30:
     print(f"Your BMI is {BMI}, you are obese.")
-elif BMI >= 25:
+elif BMI >  25:
     print(f"Your BMI is {BMI}, you are slightly overweight.")
-elif BMI >= 18.5:
+elif BMI > 18.5:
     print(f"Your BMI is {BMI}, you have a normal weight.")
 else:
       print(f"Your BMI is {BMI}, you are underweight.\nTry to dey chop.")
